29970d7ee4cb9c1c973c798de94d9fba6c571b1a
Ellu Maar <ellu.maar@gmail.com>
2024-08-08 22:51:48 +0300

    create project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import { useChat } from 'ai/react';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col w-full h-lvh max-w-md p-4 mx-auto my-4 border-2 relative rounded-lg">
+      <div className="bg-indigo-900 h-28 rounded-t-lg -m-4 flex items-center p-4">
+        <h1 className="text-3xl font-medium text-white">Chatbot</h1>
+      </div>
       {messages.map(m => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === 'user' ? 'User: ' : 'AI: '}
@@ -13,13 +16,16 @@ export default function Chat() {
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      <form className="absolute bottom-0 w-full -m-4" onSubmit={handleSubmit}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="text-lg w-full max-w-md p-2 border border-gray-300 rounded-b-lg shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Write your message"
           onChange={handleInputChange}
         />
+        <button className="absolute bottom-0 right-2 bg-indigo-700 hover:bg-indigo-900 text-white text-lg font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>
+          Send
+        </button>
       </form>
     </div>
   );
